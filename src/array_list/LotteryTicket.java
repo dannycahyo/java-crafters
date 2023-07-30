6e8134efff780d9ec0a122fc652b18710b297526
dannycahyo <dannydwicahyono@gmail.com>
2023-07-30 16:15:42 +0700

    fix: Change List into ArrayList

--- src/array_list/LotteryTicket.java
+++ src/array_list/LotteryTicket.java
@@ -1,11 +1,10 @@
 package array_list;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class LotteryTicket {
     public static void main(String[] args) {
-        List<Integer> lotteryTicket = new ArrayList<>();
+        ArrayList<Integer> lotteryTicket = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
             int random = (int) (Math.random() * 49 + 1);
